76b2f4a587fd63bdc36dac562b56beca2fb79265
Ahmed Sameh Azouz <113038422+Ahmedsamehm@users.noreply.github.com>
2026-06-20 20:33:41 +0300

    feat: initialize NestJS application entry point for Vercel serverless deployment

--- lms-backend/api/index.ts
+++ lms-backend/api/index.ts
@@ -1,34 +1,72 @@
+import 'tsconfig-paths/register';
 import { NestFactory } from '@nestjs/core';
-
 import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
+import { AppModule } from '../src/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { TransformInterceptor } from '../src/common/interceptors/transform.interceptor';
+import { Reflector } from '@nestjs/core';
+import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
-import { corsOrigins } from 'src/core/config/env';
-import { AppModule } from 'src/app.module';
+import express from 'express';
+import { corsOrigins } from '../src/core/config/env';
 
 const server = express();
 
-let cachedApp;
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
 
-async function bootstrapServer() {
+async function createApp() {
     const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
     app.use(cookieParser());
 
+    // MUST have cross-origin policy for Vercel cookies!
+    app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }));
+
     app.enableCors({
         origin: corsOrigins,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+        allowedHeaders: ['Authorization', 'Content-Type', 'Accept', 'x-lang', 'accept-language', 'stripe-signature'],
         credentials: true,
     });
 
-    await app.init();
+    app.setGlobalPrefix('api');
 
-    return server;
+    app.use(
+        express.json({
+            verify: (req: any, res, buf) => {
+                req.rawBody = buf;
+            },
+        }),
+    );
+
+    app.useGlobalInterceptors(new TransformInterceptor(new Reflector()));
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            transform: true,
+            transformOptions: {
+                enableImplicitConversion: true,
+            },
+            stopAtFirstError: true,
+        }),
+    );
+
+    await app.init();
+    return app;
 }
 
-export default async function handler(req, res) {
-    if (!cachedApp) {
-        cachedApp = await bootstrapServer();
-    }
+let cachedApp: Awaited<ReturnType<typeof createApp>> | null = null;
 
-    return cachedApp(req, res);
+// THIS is what Vercel runs
+export default async function handler(req: any, res: any) {
+    if (!cachedApp) {
+        cachedApp = await createApp();
+    }
+    server(req, res);
 }
