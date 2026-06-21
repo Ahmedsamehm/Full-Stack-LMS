b92f424a3084ddf26de3cbbbacc24bad51f7858f
Ahmed Sameh Azouz <113038422+Ahmedsamehm@users.noreply.github.com>
2026-06-21 07:36:21 +0300

    feat: implement authentication guard for protected routes via TanStack Router beforeLoad hook

--- lms-frontend/src/routes/_protected/route.tsx
+++ lms-frontend/src/routes/_protected/route.tsx
@@ -2,13 +2,10 @@ import { getUser } from '#/features/users/_api/users'
 import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_protected')({
-  beforeLoad: async () => {
-    const user = await getUser()
+  beforeLoad: async ({ context }) => {
+    const user = await context.queryClient.ensureQueryData({ queryKey: ['user'], queryFn: getUser })
 
-    if (!user) {
-      throw redirect({ to: '/login' })
-    }
-
+    if (!user) throw redirect({ to: '/login' })
     return { user }
   },
 
